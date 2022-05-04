7ee58a1250baa011b9733481c33022123530b4b5
YanisLandrieu <91880045+YanisLandrieu@users.noreply.github.com>
2022-05-04 10:16:20 +0200

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,23 +9,33 @@ let appVue = new Vue ({
         feels:'',
         pression:'',
         humidity:'',
+        regex: /^[A-Za-z]+$/,
+        error:false
 
     },
     methods: {
         makeRequest() {
         axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=09428684b8272cbafc39db375ecb991d')
           .then(response => this.request = response.data.main)
+          this.cityame = this.request.name
+      },
+      celsius() {
+          this.temp = parseInt(this.request.temp-274.15)
           this.min = parseInt(this.request.temp_min-274.15)
           this.max = parseInt(this.request.temp_max-274.15)
           this.feels = parseInt(this.request.feels_like-274.15)
           this.pression = this.request.pressure
           this.humidity = this.request.humidity
-          this.cityame = this.request.name
+          
       },
-      celsius() {
-          return  this.temp = parseInt(this.request.temp-274.15)
+      check() {
+        if(this.regex.test(this.cityName)) {
+            return this.error = true
+        }
+        else {
+            return this.error
+        }
       }
     },
-    
-    
+      
 })
