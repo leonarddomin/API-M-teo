de7c791bfdbb4d33088ebba5eb0d0bf883bb89d2
Léonard <91879922+leonarddomin@users.noreply.github.com>
2022-05-04 10:39:16 +0200

    display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,45 @@
-let appVue = new Vue ({
+let appVue = new Vue({
     el: '#app',
     data: {
         cityName: '',
-        request:'',
-        min:'',
-        max:'',
-        temp:'',
-        feels:'',
-        pression:'',
-        humidity:'',
+        request: '',
+        min: '',
+        max: '',
+        temp: '',
+        feels: '',
+        pression: '',
+        humidity: '',
         regex: /^[A-Za-z]+$/,
-        error:false
+        error: false,
+        display: false,
 
     },
     methods: {
         makeRequest() {
-        axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=09428684b8272cbafc39db375ecb991d')
-          .then(response => this.request = response.data.main)
-          this.cityame = this.request.name
-      },
-      celsius() {
-          this.temp = parseInt(this.request.temp-274.15)
-          this.min = parseInt(this.request.temp_min-274.15)
-          this.max = parseInt(this.request.temp_max-274.15)
-          this.feels = parseInt(this.request.feels_like-274.15)
-          this.pression = this.request.pressure
-          this.humidity = this.request.humidity
-          
-      },
-      check() {
-        if(this.regex.test(this.cityName)) {
-            return this.error = true
+            axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=09428684b8272cbafc39db375ecb991d')
+                .then(response => this.request = response.data.main)
+            this.cityame = this.request.name
+        },
+        displayForm: function() {
+            this.display = !this.display
+        },
+        celsius() {
+            this.temp = parseInt(this.request.temp - 274.15)
+            this.min = parseInt(this.request.temp_min - 274.15)
+            this.max = parseInt(this.request.temp_max - 274.15)
+            this.feels = parseInt(this.request.feels_like - 274.15)
+            this.pression = this.request.pressure
+            this.humidity = this.request.humidity
+
+        },
+        check() {
+            if (this.regex.test(this.cityName)) {
+                return this.error = true
+            }
+            else {
+                return this.error
+            }
         }
-        else {
-            return this.error
-        }
-      }
     },
-      
+
 })
